9911d362288335e3c2722cac9d2e6c7645578d78
huhengfeng <842671591@qq.com>
2026-07-20 17:49:26 +0800

    更新

---
+++ assets/lineChart.js
@@ -0,0 +1,84 @@
+/**
+ * 每期还款总额走势折线图配置
+ * 依赖：由页面传入 colors / rateAxis / rateSeries / rateLegend / fmtRate
+ */
+(function (global) {
+  function buildLineChartOption({
+    data,
+    labels,
+    colors,
+    rateAxis,
+    rateSeries,
+    rateLegend,
+    fmtRate,
+    getRate,
+  }) {
+    const lineRates = data.map((d) => getRate(d));
+
+    return {
+      color: [colors.total, colors.principal, colors.interest, colors.rate],
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: { type: 'cross' },
+        formatter(params) {
+          const i = params[0].dataIndex;
+          let html = `${params[0].axisValue}（${data[i].date}）<br/>`;
+          for (const p of params) {
+            if (p.seriesName === '利率') {
+              html += `${p.marker}${p.seriesName}：${fmtRate(p.value)}<br/>`;
+            } else {
+              html += `${p.marker}${p.seriesName}：${Number(p.value).toFixed(2)}<br/>`;
+            }
+          }
+          return html;
+        },
+      },
+      legend: { ...rateLegend, data: ['总额', '本金', '利息', '利率'] },
+      grid: { left: 56, right: 56, top: 40, bottom: 48 },
+      dataZoom: [{ type: 'inside', start: 0, end: 100 }],
+      xAxis: {
+        type: 'category',
+        data: labels,
+        boundaryGap: false,
+        axisLabel: { rotate: 40, fontSize: 11 },
+      },
+      yAxis: [
+        {
+          type: 'value',
+          splitLine: { lineStyle: { type: 'dashed', color: '#d8e0ea' } },
+        },
+        rateAxis,
+      ],
+      series: [
+        {
+          name: '总额',
+          type: 'line',
+          smooth: true,
+          symbol: 'none',
+          lineStyle: { width: 2.5 },
+          data: data.map((d) => d.total),
+        },
+        {
+          name: '本金',
+          type: 'line',
+          smooth: true,
+          symbol: 'none',
+          lineStyle: { width: 1.5 },
+          data: data.map((d) => d.principal),
+        },
+        {
+          name: '利息',
+          type: 'line',
+          smooth: true,
+          symbol: 'none',
+          lineStyle: { width: 1.5 },
+          data: data.map((d) => d.interest),
+        },
+        rateSeries(lineRates),
+      ],
+    };
+  }
+
+  global.GjjCharts = global.GjjCharts || {};
+  global.GjjCharts.buildLineChartOption = buildLineChartOption;
+})(typeof window !== 'undefined' ? window : globalThis);
